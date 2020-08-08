9038a8ea91c1939ea169a43128de77ab21428b66
palanikaveri2012 <kpspalanimca@gmail.com>
2020-08-08 21:40:53 +0530

    added if condition to check the data

--- src/main/java/com/tcs/git_demo1/App.java
+++ src/main/java/com/tcs/git_demo1/App.java
@@ -1,16 +1,15 @@
 package com.tcs.git_demo1;
 
-
-public class App 
-{
+public class App {
 	public void addData(String data) {
-		
-		
-		System.out.println("data:"+data);
+
+		System.out.println("data:" + data);
+		if (data.equals("java techie")) {
+			System.out.println("Welcome");
+		}
 	}
-	
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
